851df4acf3b9aa855ae46c745f73098c7171e14f
Kushan Pabasara <41328077+Kushanpabasara@users.noreply.github.com>
2020-01-15 23:47:04 +0530

    updated

--- src/environments.js
+++ src/environments.js
@@ -1,5 +1,5 @@
 export const environment = {
   //   baseUrl: 'http://localhost:10000/api/', // local
   //baseUrl: 'http://54.251.247.220:10000/api/', //live
-  baseUrl: 'http://trumpcorebackend.trumpcode.com:10000/api/', //ssl
+  baseUrl: 'https://trumpcorebackend.trumpcode.com:10003/api/', //ssl
 };
