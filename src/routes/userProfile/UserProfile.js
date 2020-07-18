52092c352ada3405a5ec79a5fc3625c6331a0fa3
Kushan Pabasara <41328077+Kushanpabasara@users.noreply.github.com>
2020-07-18 17:38:32 +0530

    add subscription for user setting

--- src/routes/userProfile/UserProfile.js
+++ src/routes/userProfile/UserProfile.js
@@ -56,86 +56,116 @@ class Data extends React.Component {
   render() {
     const { getFieldDecorator } = this.props.form;
     return (
-      <div className="container-fluid no-breadcrumb container-mw chapter">
-        <article className="article">
-          <div className="row">
-            <div className="col-lg-4 mb-4">
-              <article className="profile-card-v2 h-100">
-                <h4>Profile Details</h4>
-                <div className="divider divider-solid my-4" />
-                <div
-                  style={{
-                    display: 'inline-flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                  }}
-                >
-                  <img
-                    src={picture_avatar}
-                    alt="avatar"
-                    style={{ width: '100%' }}
-                    className="no_border"
-                  />
-                </div>
-                <h3 className="mt-2">{this.currentUser && this.currentUser.accName}</h3>
-                <div className="mt-2 mb-4"></div>
-              </article>
-            </div>
+      <>
+        <div className="container-fluid no-breadcrumb container-mw chapter">
+          <article className="article">
+            <div className="row">
+              <div className="col-lg-4 mb-4">
+                <article className="profile-card-v2 h-100">
+                  <h4>Profile Details</h4>
+                  <div className="divider divider-solid my-4" />
+                  <div
+                    style={{
+                      display: 'inline-flex',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}
+                  >
+                    <img
+                      src={picture_avatar}
+                      alt="avatar"
+                      style={{ width: '100%' }}
+                      className="no_border"
+                    />
+                  </div>
+                  <h3 className="mt-2">{this.currentUser && this.currentUser.accName}</h3>
+                  <div className="mt-2 mb-4"></div>
+                </article>
+              </div>
 
-            <div className="col-lg-8 mb-8">
-              <article className="profile-card-v2 h-100">
-                <h4>Change Password</h4>
-                <div className="divider divider-solid my-4" />
-                <div>
-                  <Form onSubmit={this.handleSubmit}>
-                    <Row gutter={24}>
-                      <Col span={8} order={1}>
-                        <Form.Item label="Password" hasFeedback>
-                          {getFieldDecorator('password', {
-                            rules: [
-                              {
-                                required: true,
-                                message: 'Please input your password!',
-                              },
-                              {
-                                validator: this.validateToNextPassword,
-                              },
-                              { min: 5, message: 'Password must be minimum 5 characters.' },
-                            ],
-                          })(<Input.Password />)}
-                        </Form.Item>
-                      </Col>
-                      <Col span={8} order={2}>
-                        <Form.Item label="Confirm Password" hasFeedback>
-                          {getFieldDecorator('confirm', {
-                            rules: [
-                              {
-                                required: true,
-                                message: 'Please confirm your password!',
-                              },
-                              {
-                                validator: this.compareToFirstPassword,
-                              },
-                              { min: 5, message: 'Password must be minimum 5 characters.' },
-                            ],
-                          })(<Input.Password onBlur={this.handleConfirmBlur} />)}
-                        </Form.Item>
-                      </Col>
-                      <Col span={8} order={3}>
-                        <Form.Item>
-                          <Button className="mt-4" type="primary" htmlType="submit">
-                            Change
-                          </Button>
-                        </Form.Item>
-                      </Col>
-                    </Row>
-                  </Form>
-                </div>
-              </article>
+              <div className="col-lg-8 mb-8">
+                <article className="profile-card-v2 h-100">
+                  <h4>Change Password</h4>
+                  <div className="divider divider-solid my-4" />
+                  <div>
+                    <Form onSubmit={this.handleSubmit}>
+                      <Row gutter={24}>
+                        <Col span={8} order={1}>
+                          <Form.Item label="Password" hasFeedback>
+                            {getFieldDecorator('password', {
+                              rules: [
+                                {
+                                  required: true,
+                                  message: 'Please input your password!',
+                                },
+                                {
+                                  validator: this.validateToNextPassword,
+                                },
+                                { min: 5, message: 'Password must be minimum 5 characters.' },
+                              ],
+                            })(<Input.Password />)}
+                          </Form.Item>
+                        </Col>
+                        <Col span={8} order={2}>
+                          <Form.Item label="Confirm Password" hasFeedback>
+                            {getFieldDecorator('confirm', {
+                              rules: [
+                                {
+                                  required: true,
+                                  message: 'Please confirm your password!',
+                                },
+                                {
+                                  validator: this.compareToFirstPassword,
+                                },
+                                { min: 5, message: 'Password must be minimum 5 characters.' },
+                              ],
+                            })(<Input.Password onBlur={this.handleConfirmBlur} />)}
+                          </Form.Item>
+                        </Col>
+                        <Col span={8} order={3}>
+                          <Form.Item>
+                            <Button className="mt-4" type="primary" htmlType="submit">
+                              Change
+                            </Button>
+                          </Form.Item>
+                        </Col>
+                      </Row>
+                    </Form>
+                  </div>
+                </article>
+              </div>
             </div>
-          </div>
-        </article>
-      </div>
+          </article>
+          <article className="article">
+            <div className="row">
+              <div className="col-lg-4 mb-4">
+                <article className="profile-card-v2 h-100">
+                  <h4>Subscription Details</h4>
+                  <div className="divider divider-solid my-4" />
+                  <div className="mt-2 mb-4">
+                    <Form>
+                      <Row gutter={24}>
+                        <Col span={24} order={1}>
+                          <Form.Item label="Subscription ID" hasFeedback>
+                            {getFieldDecorator('subId', {
+                              rules: [
+                                {
+                                  required: true,
+                                  message: 'Please input your password!',
+                                },
+                              ],
+                            })(<Input />)}
+                          </Form.Item>
+                        </Col>
+                      </Row>
+                    </Form>
+                  </div>
+                </article>
+              </div>
+            </div>
+          </article>
+        </div>
+      </>
     );
   }
 }
