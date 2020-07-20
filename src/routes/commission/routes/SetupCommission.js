0254af26fca56f6488fbc26bd2bfaa59ef7e6889
Kushan Pabasara <41328077+Kushanpabasara@users.noreply.github.com>
2020-07-20 19:39:45 +0530

    update commission setup

--- src/routes/commission/routes/SetupCommission.js
+++ src/routes/commission/routes/SetupCommission.js
@@ -21,19 +21,51 @@ import {
   AutoComplete,
   Spin,
   Switch,
+  Typography,
 } from 'antd';
 
 const FormItem = Form.Item;
 const Search = Input.Search;
+const { Option } = Select;
 const { Column, ColumnGroup } = Table;
+const { Text, Link } = Typography;
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+
+const children = [];
+for (let i = 1; i < 4; i++) {
+  children.push(
+    <Option key={i} value={'Dcode' + i}>
+      {'DCode' + i}
+    </Option>
+  );
+}
 
 class Data extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      dCode: [],
+    };
   }
+
+  handleChange = value => {
+    console.log(`selected ${value}`);
+    this.setState({
+      dCode: value,
+    });
+  };
+
   render() {
     const { getFieldDecorator } = this.props.form;
+
     return (
       <div className="container-fluid no-breadcrumb container-mw chapter">
         <QueueAnim type="bottom" className="ui-animate">
@@ -44,41 +76,152 @@ class Data extends React.Component {
                 <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, 20]}>
                   <Col xs={24} sm={12} md={12} lg={6}>
                     <FormItem label="Agent Type Code">
-                      {getFieldDecorator('agentTypeCode', {
+                      {getFieldDecorator('agentType', {
                         rules: [
                           {
                             required: true,
-                            message: 'Agent type code cannot be empty.',
+                            message: 'Please enter your agent type',
                           },
                         ],
-                      })(<Input style={{ width: '100%' }} />)}
+                      })(
+                        <AutoComplete
+                          //dataSource={}
+                          style={{ width: '100%' }}
+                          placeholder="Agent Number"
+                          // onBlur={inputValue => {
+                          //   this.setAccount(inputValue);
+                          // }}
+                          // onChange={inputValue => {
+                          //   this.updateAccountList(inputValue);
+                          // }}
+                          // onSelect={inputValue => {
+                          //   this.setAccount(inputValue);
+                          // }}
+                        />
+                      )}
                     </FormItem>
                   </Col>
                   <Col xs={24} sm={12} md={12} lg={6}>
-                    <FormItem label="Channel Type">
+                    <FormItem label="Channel Type Code">
                       {getFieldDecorator('channelType', {
                         rules: [
                           {
                             required: true,
-                            message: 'Channel type cannot be empty.',
+                            message: 'Please enter your channel type',
                           },
                         ],
-                      })(<Input style={{ width: '100%' }} />)}
+                      })(
+                        <AutoComplete
+                          //dataSource={}
+                          style={{ width: '100%' }}
+                          placeholder="Channel Type"
+                          // onBlur={inputValue => {
+                          //   this.setAccount(inputValue);
+                          // }}
+                          // onChange={inputValue => {
+                          //   this.updateAccountList(inputValue);
+                          // }}
+                          // onSelect={inputValue => {
+                          //   this.setAccount(inputValue);
+                          // }}
+                        />
+                      )}
                     </FormItem>
                   </Col>
                   <Col xs={24} sm={12} md={12} lg={6}>
                     <FormItem label="Transaction Type Code">
-                      {getFieldDecorator('tc_TransactionTypeCode', {
+                      {getFieldDecorator('transactionType', {
                         rules: [
                           {
                             required: true,
-                            message: 'Transaction type code cannot be empty.',
+                            message: 'Please enter your transaction type',
                           },
                         ],
-                      })(<Input style={{ width: '100%' }} />)}
+                      })(
+                        <AutoComplete
+                          //dataSource={}
+                          style={{ width: '100%' }}
+                          placeholder="Transaction Type"
+                          // onBlur={inputValue => {
+                          //   this.setAccount(inputValue);
+                          // }}
+                          // onChange={inputValue => {
+                          //   this.updateAccountList(inputValue);
+                          // }}
+                          // onSelect={inputValue => {
+                          //   this.setAccount(inputValue);
+                          // }}
+                        />
+                      )}
                     </FormItem>
                   </Col>
+                  <Col xs={24} sm={12} md={12} lg={6}>
+                    <FormItem label="Distribution Code">
+                      {getFieldDecorator('distributionCode', {
+                        rules: [
+                          {
+                            required: true,
+                            message: 'Please enter your Distribution codes',
+                          },
+                        ],
+                      })(
+                        <Select
+                          mode="multiple"
+                          style={{ width: '100%' }}
+                          placeholder="Please select"
+                          //defaultValue={['a10', 'c12']}
+                          onChange={this.handleChange}
+                        >
+                          {children}
+                        </Select>
+                      )}
+                    </FormItem>
+                    {console.log(this.state.dCode)}
+                  </Col>
                 </Row>
+                {this.state.dCode.length != 0 && (
+                  <>
+                    {this.state.dCode.map((item, i) => (
+                      <Row key={i}>
+                        <Col className="mt-2" span={4}>
+                          <Text strong>{item}</Text>
+                        </Col>
+                        <Col span={6}>
+                          <FormItem label="Fixed" {...layout}>
+                            {getFieldDecorator(item + 'Fixed', {
+                              rules: [
+                                {
+                                  required: true,
+                                  message: 'Please enter your Distribution codes',
+                                },
+                              ],
+                            })(
+                              <InputNumber
+                                initialValue={100}
+                                min={0}
+                                max={100}
+                                formatter={value => `${value}%`}
+                                parser={value => value.replace('%', '')}
+                              />
+                            )}
+                          </FormItem>
+                        </Col>
+                        <Col span={6}>
+                          <FormItem label="Amount" {...layout}>
+                            {getFieldDecorator(item + 'Amount', {
+                              rules: [
+                                {
+                                  required: true,
+                                  message: 'Please enter your Distribution codes',
+                                },
+                              ],
+                            })(<InputNumber initialValue={100} min={0} step={0.1} />)}
+                          </FormItem>
+                        </Col>
+                      </Row>
+                    ))}
+                  </>
+                )}
               </Form>
             </div>
           </div>
